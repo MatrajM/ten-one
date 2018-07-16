e7bba996e9b33669a3a2c41c1e1adc4c2bb46648
Michał Matraj <matraj.michal@op.pl>
2018-07-16 18:10:42 +0200

    Add crousel

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var elem = document.querySelector('.main-carousel');
+
+var flkty = new Flickity( elem, {
+    groupCells: true
+  });
+  
+  var buttonGroup = document.querySelector('.button-group');
+  var buttons = buttonGroup.querySelectorAll('.button');
+  buttons = fizzyUIUtils.makeArray( buttons );
+  
+  buttonGroup.addEventListener( 'click', function( event ) {
+    
+    if ( !matchesSelector( event.target, '.button' ) ) {
+      return;
+    }
+    var index = buttons.indexOf( event.target );
+    flkty.selectCell( index );
+  });
+  
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on( 'scroll', function( progress ) {
+  progress = Math.max( 0, Math.min( 1, progress ) );
+  progressBar.style.width = progress * 100 + '%';
+});
